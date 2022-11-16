39c80d02e6c189ab7ffde9431cad662175401371
RicardoKd <richardo.kadyan@gmail.com>
2022-11-16 16:31:06 +0200

    Robot tracker component v0.0.0

---
+++ src/Components/RobotTracker/index.tsx
@@ -0,0 +1,17 @@
+import style from "./index.module.css";
+
+const RobotTracker = () => {
+
+
+  const warehouseRepresentations = () => {
+    for (let index = 0; index < 50; index++) {}
+  };
+
+  return (
+    <div className={style.RobotTrackerWrapper}>
+      {/* <div>{warehouseRepresentations}</div> */}
+    </div>
+  );
+};
+
+export default RobotTracker;
