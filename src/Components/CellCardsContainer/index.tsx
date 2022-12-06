bcc8fdf56e032c17edc89965d96e3da54f6e8df5
RicardoKd <richardo.kadyan@gmail.com>
2022-12-07 00:07:46 +0200

    removed loggin

--- src/Components/CellCardsContainer/index.tsx
+++ src/Components/CellCardsContainer/index.tsx
@@ -5,7 +5,6 @@ import CellCard from "../CellCard";
 import styles from "./index.module.css";
 
 const CellCardsContainer = () => {
-  // console.log("Cards container render");
   const rentedCells = useAppSelector(selectRentedCells);
 
   const emptyRentedCellsCount = useMemo(() => {
